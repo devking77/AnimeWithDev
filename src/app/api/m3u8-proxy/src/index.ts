110817ae970667828e49aa1024d108e4a3f77b31
Devender Saini <devs97111@gmail.com>
2025-03-26 14:04:24 +0530

    error resolved

--- src/app/api/m3u8-proxy/src/index.ts
+++ src/app/api/m3u8-proxy/src/index.ts
@@ -1,22 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { router } from './routes/route';
-import { cacheRoutes } from "./utils/cache-routes";
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import { router } from './routes/route';
+// import { cacheRoutes } from "./utils/cache-routes";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-const PORT = 4040;
+// const app = express();
+// const PORT = 4040;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "*" }));
-app.use(cacheRoutes());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors({ origin: "*" }));
+// app.use(cacheRoutes());
 
-app.get("/", (_, res) => { res.send("hianime streaming m3u8 proxy") });
-app.use('/', router);
+// app.get("/", (_, res) => { res.send("hianime streaming m3u8 proxy") });
+// app.use('/', router);
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
-export default app;
+// export default app;
--- src/app/api/m3u8-proxy/src/routes/route.ts
+++ src/app/api/m3u8-proxy/src/routes/route.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import { m3u8Proxy } from '../controllers/m3u8-proxy';
+// import express from 'express';
+// import { m3u8Proxy } from '../controllers/m3u8-proxy';
 
-export const router = express.Router();
+// export const router = express.Router();
 
-router.get('/m3u8-proxy', m3u8Proxy);
+// router.get('/m3u8-proxy', m3u8Proxy);
